3c1712bbced6458f3f5fc501c82cbc221e8b1f91
Yatharth SATIJA <ysatija2-c@my.cityu.edu.hk>
2024-10-17 12:00:57 +0800

    FIx imports

--- src/app/merchant/orders/page.tsx
+++ src/app/merchant/orders/page.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import Link from 'next/link';
 import { Card } from 'primereact/card';
-import { Image } from 'primereact/image';
 import { Button } from 'primereact/button';
         
 
